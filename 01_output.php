61f8978a15d7f1c32abc2a3da7594604bb499089
Himangsu <himangsusarkar1@gmail.com>
2022-04-11 09:36:26 +0800

    create php enviroment

---
+++ 01_output.php
@@ -0,0 +1,19 @@
+<?php
+//echo-output strings,number,html etc.
+//echo 123,'hello',10.5;
+//print- works echo,but can only take in a single arguments
+//print 123;
+
+//print_r()-print single values and arrays
+//print-r([1,2,3]);
+
+//var_dump()-returns more info like data type and length
+//var_dump(true);
+
+//var_export()-similar to var_dump(), outputs a string represents of a variable
+//var_export('hello');
+
+echo 'Hello';
+
+ ?>
+
