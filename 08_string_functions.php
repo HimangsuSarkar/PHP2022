4a867acf513c1f6703dcc5d3b9fde4a18afa8b2d
Himangsu <himangsusarkar1@gmail.com>
2022-04-21 14:03:57 +0800

    basic string functions

---
+++ 08_string_functions.php
@@ -0,0 +1,42 @@
+<?php
+$string='Hello World';
+
+//get the length of string
+echo strlen($string);
+
+//find the position of the first occurrence of a substring in a string
+echo strpos($string,'o');
+
+//find the position of the last occurrence of a substring in a string
+echo strrpos($string,'o');
+
+//reverse a string
+echo strrev($string);
+
+
+//convert all characters to lowercase
+echo strtolower($string);
+
+//convert all characters to uppercase
+echo strtoupper($string);
+
+//uppercase first character of each word
+echo ucwords($string);
+
+//string replace
+echo str_replace('World','Everyone',$string);
+
+//return portion of a string specified by the offset and length
+echo substr($string,0,5);
+echo substr($string,5);
+
+//starts with
+if(str_starts_with($string,'Hello')){
+  echo "YES";
+};
+
+//end with
+
+if(str_ends_with($string,'id')){
+  echo "yes";
+};
